30aabbeddbae5bb68fc4c50e41daaa3e4bb4b726
Maiko.T.S <maiko.t.s.ofc@gmail.com>
2026-08-27 12:05:37 -0300

    Modify EULA acceptance behavior and clean up logs
    Changed EULA acceptance default from true to false and removed logging statements.

--- main.cpp
+++ main.cpp
@@ -9,28 +9,17 @@ ConfigEntry* pEULAAccepted;
 
 extern "C" void OnModLoad()
 {
-    // Usando o log padrão do aml
-    aml->Log("=== AcceptEULA Mod Carregado ===");
-
     pSCAndLib = aml->GetLib("libSCAnd.so");
     if(pSCAndLib)
     {
-        aml->Log("Biblioteca libSCAnd.so encontrada com sucesso!");
-
-        pEULAAccepted = cfg->Bind("AcceptEULA", true);
+        pEULAAccepted = cfg->Bind("AcceptEULA", false);
         if(pEULAAccepted->GetBool())
         {
-            aml->Log("Aplicando patch para aceitar a EULA automaticamente...");
-            
             aml->Unprot(pSCAndLib + 0x31C149, 1);
             *(bool*)(pSCAndLib + 0x31C149) = true;
-            
-            aml->Log("Patch aplicado com sucesso!");
             return;
         }
-    }
-    else
-    {
-        aml->Log("Erro: libSCAnd.so não foi encontrada!");
+        pEULAAccepted->SetBool(true);
+        cfg->Save();
     }
 }

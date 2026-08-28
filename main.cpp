ba905d997b4043cf957765801c43a05504b9d64e
Maiko.T.S <maiko.t.s.ofc@gmail.com>
2026-08-28 02:33:33 -0300

    Atualizar o main.cpp

--- main.cpp
+++ main.cpp
@@ -12,14 +12,14 @@ extern "C" void OnModLoad()
     pSCAndLib = aml->GetLib("libSCAnd.so");
     if(pSCAndLib)
     {
-        pEULAAccepted = cfg->Bind("AcceptEULA", false);
-        if(pEULAAccepted->GetBool())
-        {
-            aml->Unprot(pSCAndLib + 0x31C149, 1);
-            *(bool*)(pSCAndLib + 0x31C149) = true;
-            return;
-        }
-        pEULAAccepted->SetBool(true);
+        // Vincula ou cria a entrada na config
+        pEULAAccepted = cfg->Bind("AcceptEULA", true);
+
+        // Aplica a modificação na memória do libSCAnd.so
+        aml->Unprot(pSCAndLib + 0x31C149, 1);
+        *(bool*)(pSCAndLib + 0x31C149) = true;
+
+        // Salva o estado atualizado da config
         cfg->Save();
     }
 }

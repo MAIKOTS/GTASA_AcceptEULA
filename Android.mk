0b7d4b1497c54479f114c39b36719ccb2b2a5c03
Maiko.T.S <maiko.t.s.ofc@gmail.com>
2026-09-20 13:21:10 -0300

    Add files via upload

--- Android.mk
+++ Android.mk
@@ -1,26 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
 LOCAL_CPP_EXTENSION := .cpp .cc
-LOCAL_MODULE := AcceptEULA
-LOCAL_SRC_FILES := main.cpp
-
-LOCAL_CFLAGS := \
-    -O2 \
-    -mfloat-abi=softfp \
-    -DNDEBUG \
-    -std=c++17 \
-    -ffunction-sections \
-    -fdata-sections
-
-LOCAL_C_INCLUDES := \
-    $(LOCAL_PATH)
-
-LOCAL_LDLIBS := -llog
-
-LOCAL_LDFLAGS := \
-    -Wl,--gc-sections \
-    -Wl,--strip-unneeded
-
+LOCAL_MODULE    := AcceptEULA
+LOCAL_SRC_FILES := main.cpp mod/config.cpp
+LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17
+LOCAL_C_INCLUDES += ./include
+LOCAL_LDLIBS += -llog # ARM64 library requires it so...
 include $(BUILD_SHARED_LIBRARY)

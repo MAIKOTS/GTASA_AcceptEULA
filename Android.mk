35f962da25a6b7bc60041aac22bd7476359ffe72
Maiko.T.S <maiko.t.s.ofc@gmail.com>
2026-09-20 13:07:49 -0300

    Update Android.mk for improved build settings
    Refactor Android.mk to improve build configuration and optimization flags.

--- Android.mk
+++ Android.mk
@@ -1,10 +1,26 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+
 LOCAL_CPP_EXTENSION := .cpp .cc
-LOCAL_MODULE    := AcceptEULA
-LOCAL_SRC_FILES := main.cpp mod/config.cpp
-LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17
-LOCAL_C_INCLUDES += ./include
-LOCAL_LDLIBS += -llog # ARM64 library requires it so...
+LOCAL_MODULE := AcceptEULA
+LOCAL_SRC_FILES := main.cpp
+
+LOCAL_CFLAGS := \
+    -O2 \
+    -mfloat-abi=softfp \
+    -DNDEBUG \
+    -std=c++17 \
+    -ffunction-sections \
+    -fdata-sections
+
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)
+
+LOCAL_LDLIBS := -llog
+
+LOCAL_LDFLAGS := \
+    -Wl,--gc-sections \
+    -Wl,--strip-unneeded
+
 include $(BUILD_SHARED_LIBRARY)
